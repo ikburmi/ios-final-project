f407dbbe1dc9bee7c3ce75a3f5f7a0c9b395e441
Ikroop Burmi <ikburmi@gmail.com>
2026-04-16 23:01:30 -0400

    Initial Commit

---
+++ final-project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  final-project
+//
+//  Created by Ikroop Burmi on 4/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ final-project/final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  final_projectApp.swift
+//  final-project
+//
+//  Created by Ikroop Burmi on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
